34c5137759372cd24559e8b08350d56b917a885a
Vedant Mathur <94904098+vedantmathur13@users.noreply.github.com>
2024-08-09 22:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Simple alert on form submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
